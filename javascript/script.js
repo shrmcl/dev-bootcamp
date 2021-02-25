fb66555e5f8c91761897587524224b066bc431bf
Shar McLeod <shr.mcl@gmail.com>
2021-02-25 11:53:05 -0600

    project 1 complete

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var fname = prompt("Please enter your name.");
+var snack = prompt("What is your favorite snack?");
+var drink = prompt("What is your favorite drink?");
+
+document.write("Hi there, " + fname + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!")
