dc65370bd4191137af22ab7b6621f66d2ef2d75c
Shar McLeod <shr.mcl@gmail.com>
2021-03-16 13:50:58 -0500

    js project 8 functional except transition error on second hand

--- javascript/project8/script.js
+++ javascript/project8/script.js
@@ -1,18 +1,28 @@
 
 let now = new Date();
 let sec = now.getSeconds();
-
+let min = now.getMinutes();
+let hr = now.getHours();
 
 setInterval(function(){
-    let dgr = sec * 6 - 90;
-    console.log(dgr);
+    let sdgr = sec * 6 - 90;
+    let mdgr = min * 6 - 90;
+    let hdgr = hr * 30 - 90;
     
-
-    if(sec >= 60) {
+    if (sec >= 60) {
         sec = 0;
+        min++;
     }
 
-    document.querySelector("#seconds").style.transform = "translate(120px, 120px) rotate(" + dgr + "deg)";
+    if (min >= 60) {
+        hr++;
+    }
+
+    document.querySelector("#seconds").style.transform = "translate(120px, 120px) rotate(" + sdgr + "deg)";
     sec += 1;
+
+    document.querySelector("#minutes").style.transform = "translate(120px, 120px) rotate(" + mdgr + "deg)";
+
+    document.querySelector("#hours").style.transform = "translate(120px, 120px) rotate(" + hdgr + "deg)";
 
 }, 1000)
