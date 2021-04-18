0fa1329710750ba5d7f4953169b6d38a75aef412
Shar McLeod <shr.mcl@gmail.com>
2021-04-17 20:31:44 -0500

    pushing unfinished Movie Finder project

---
+++ node_api/MoviesPlaying/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const $fetch = require('node-fetch');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// TMDB api key and endpoint
+let myKey = '4655033f7ad9bef3342338a2c91e15ae';
+let url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + myKey + '&language=en-US&page=1'
+
+app.get('/getMovies', (req, res) => {
+   $fetch(url)
+   .then(res => res.json())
+   .then(json => {
+    //    console.log(json.results)
+ 
+    // json[results].forEach(element => {
+        // do stuff here 
+        // the movie title, overview, rating, and poster image. 
+   
+    // add the data variables to be passed into the ejs . i.e. {data: data}
+
+    res.render('results');
+   })
+
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log(`listening on port ${port}`)})
