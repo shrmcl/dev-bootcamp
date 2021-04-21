34e2cc37e3992ff40332c9720da08bca28d6e482
Shar McLeod <shr.mcl@gmail.com>
2021-04-21 18:55:20 -0500

    completed todo app code

--- node_api/todos/app.js
+++ node_api/todos/app.js
@@ -6,6 +6,7 @@ app.use(logger('dev'));
 
 const {toDoArray} = require('./fakeData')
 
+
 app.get('/', (req, res) => {
     res.send('am root route')
 })
@@ -34,13 +35,28 @@ app.delete('/todos/:id', (req, res) => {
     let requestedToDoIndex = toDoArray.findIndex( todo => {
         return todo.id === requestedToDoId
     })
-    console.log(requestedToDoIndex);
+    // console.log(requestedToDoIndex);
     toDoArray.splice(requestedToDoIndex, 1);
-    res.json(toDoArray);
+    res.send(toDoArray);
 
+});
 
-    
-})
-
+// Update
+app.put("/todos/:id", (req, res) => {
+    // get the requestedToDoId from req.params and ensure it is a number
+    let requestedToDoId = parseInt(req.params.id);
+  
+    // find the affected todo item
+    let requestedToDo = toDoArray.find(todo => {
+      return todo.id === requestedToDoId;
+    });
+  
+    // toggle the completion status
+    requestedToDo.isComplete = !requestedToDo.isComplete;
+  
+    // return the toggled todo as confirmation
+    res.status(201).json(requestedToDo);
+  });
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`))
