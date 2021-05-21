7fc01a16ceed304a24d3fb87bb8a66c5ea21974d
Shar McLeod <shr.mcl@gmail.com>
2021-05-21 15:03:01 -0500

    adding practice js code

---
+++ extras/practice0521/scripts.js
@@ -0,0 +1,19 @@
+// Write a JavaScript function that reverses a number. 
+// For example, x = 32243;
+// Expected Output : 34223
+
+// let num1 = 12345555508;
+// let num2 = parseInt(num1.toString().split('').reverse().join(''));
+// console.log(num2)
+
+
+// Write a JavaScript function that reverses a number. 
+// For example, x = 322.43;
+// Expected Output : 342.23      
+
+
+// Write a JavaScript function which accepts an argument and returns the type of the argument provided.
+
+function dataType (name) {
+  return typeof(name)
+}
