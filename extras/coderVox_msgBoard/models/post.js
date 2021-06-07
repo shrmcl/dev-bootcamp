4d977a6d12fe2da04f4716c3bf45736dbe4bc055
Shar McLeod <shr.mcl@gmail.com>
2021-06-07 18:14:36 -0500

    backtracking to debug

--- extras/coderVox_msgBoard/models/post.js
+++ extras/coderVox_msgBoard/models/post.js
@@ -1,41 +1,20 @@
-const mongoose = require("mongoose");
-const { post } = require("../routes/posts");
-const router = require("../routes/posts");
-const postSchema = new mongoose.Schema({
-    topic: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    }
-});
+// const mongoose = require("mongoose");
+// const { post } = require("../routes/posts");
+// const router = require("../routes/posts");
+// const postSchema = new mongoose.Schema({
+//     topic: {
+//         type: String,
+//         required: true
+//     },
+//     message: {
+//         type: String,
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-// actually this is "/posts"
-router.post("/", (req, res) => {
-    let thePost = new post({
-        topic: req.body.topic,
-        message: req.body.message,
-        name: req.body.name
-    });
-    thePost.save((error, post) => {
-        if(error) {
-            console.log(error);
-            // direct the user to the form they just submitte so changes can be made
-            res.render("new", {post: thePost})
-        } else {
-            console.log(post);
-            // direct user to the post they just successfully created
-            // id created automatically by mongo
-            res.redirect(`/posts/${post._id}`)
-        }
-    })
-});
-
-// export the schema to be accessed by posts.js
-module.exports = mongoose.model("Post", postSchema)
+// // export the schema to be accessed by posts.js
+// module.exports = mongoose.model("Post", postSchema)
--- extras/coderVox_msgBoard/routes/posts.js
+++ extras/coderVox_msgBoard/routes/posts.js
@@ -1,35 +1,16 @@
 const express = require("express");
 // Router is a built-in Express method to handle routes
 const router = express.Router();
-// import our mongoose schema from post.js
-const Post = require("./../models/post");
-// this "/" is actually "/posts" which we added in app.js via "app.use("/posts", postRoutes);"
+// route is actually "/posts" here, which we redirected root route to
 router.get("/", (req, res) => {
     const posts = [{
-        topic: "I like banana smoothies",
-        message: "Does anyone have a good recipe?",
+        topic: "i like bananas",
+        message: "does anyone have a recipe",
         name: "Linda"
-    },
-    {
-        topic: "xxx",
-        message: "ddd?",
-        name: "eee"
     }];
     res.render("index", {posts: posts});
 })
-// this is really "/posts/new"
-router.get("/new", (req, res) => {
-    // we are autopopulating form with empty strings
-    // only to handle the issue of the fact that we are also populating form in form_inputs.ejs 
-    // with unsuccessful post attempts to be corrected by user 
-    let post = {topic: "", message: "", name: ""}
-    res.render("new", {post: post});
-});
 
-// show user a success message when post is successful
-router.get("/:id", (req, res) => {
-    res.send("It works!");
-})
 
 // allow app.js to use these routes
 module.exports = router;
