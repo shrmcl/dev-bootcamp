1c2bc3b483cefa294280a22f30a71521c954fa5f
Shar McLeod <shr.mcl@gmail.com>
2021-06-07 01:13:31 -0500

    adding form and post stuff

--- extras/coderVox_msgBoard/app.js
+++ extras/coderVox_msgBoard/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 // connect our routes we created in the posts.js file
 const postRoutes = require("./routes/posts");
 const port = process.env.PORT || 3000;
 
+mongoose.connect('mongodb://localhost:27017/message_board', {useNewUrlParser: true}, {useUnifiedTopology: true} )
+
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-// first arg adds "/posts" to start of each route
+// so we can read data coming from req.body. (middleware)
+app.use(express.urlencoded({ extended: true }))
+// first arg adds "/posts" to start of each route inside posts.js
 // second arg tells app to use routes in posts.js via postRoutes const above
 app.use("/posts", postRoutes);
-
 
 app.get("/", (req, res) => {
     // res.send('connected')
     // res.render("index");
+    // when user goes to "/" (root page) they will be redirected to "/posts"
     res.redirect("/posts");
 });
 
--- extras/coderVox_msgBoard/routes/posts.js
+++ extras/coderVox_msgBoard/routes/posts.js
@@ -1,11 +1,35 @@
 const express = require("express");
 // Router is a built-in Express method to handle routes
 const router = express.Router();
-
+// import our mongoose schema from post.js
+const Post = require("./../models/post");
+// this "/" is actually "/posts" which we added in app.js via "app.use("/posts", postRoutes);"
 router.get("/", (req, res) => {
-    res.render("index");
+    const posts = [{
+        topic: "I like banana smoothies",
+        message: "Does anyone have a good recipe?",
+        name: "Linda"
+    },
+    {
+        topic: "xxx",
+        message: "ddd?",
+        name: "eee"
+    }];
+    res.render("index", {posts: posts});
 })
+// this is really "/posts/new"
+router.get("/new", (req, res) => {
+    // we are autopopulating form with empty strings
+    // only to handle the issue of the fact that we are also populating form in form_inputs.ejs 
+    // with unsuccessful post attempts to be corrected by user 
+    let post = {topic: "", message: "", name: ""}
+    res.render("new", {post: post});
+});
 
+// show user a success message when post is successful
+router.get("/:id", (req, res) => {
+    res.send("It works!");
+})
 
 // allow app.js to use these routes
 module.exports = router;
