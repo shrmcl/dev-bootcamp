1c5856e7b9e9c4990cb028c62f99994ab30d5fcf
Shar McLeod <shr.mcl@gmail.com>
2021-06-19 12:12:51 -0500

    pushing fully functional recipe app

--- react/demos/recipe-props/src/RecipeApp.js
+++ react/demos/recipe-props/src/RecipeApp.js
@@ -3,18 +3,14 @@ import React, { Component } from 'react';
 // import './RecipeApp.css';
 import Recipe from './Recipe';
 import Navbar from './Navbar';
+import RecipeList from './RecipeList';
 
 class RecipeApp extends Component {
  render() {
    return (
      <div className="App">
        <Navbar />
-       <Recipe 
-       title="pasta" 
-        ingredients={['flour', 'water']} 
-        instructions="Mix ingredients"
-        img="https://i.imgur.com/2q2OgnC.jpg"
-      />
+       <RecipeList/>
      </div>
    );
  }
