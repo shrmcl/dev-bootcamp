1175d75b2c8159ccde43ab680d860b5123e6d60f
Shar McLeod <shr.mcl@gmail.com>
2021-06-29 12:12:39 -0500

    added parser and form values. req.body returns accurate data.

--- extras/volApp_test1/app.js
+++ extras/volApp_test1/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+// to parse the POST form data, use this parser
+// "true" instead of "false" fixed the issue of getting body: "[Object: null prototype] " returned before req.body data
+app.use(express.urlencoded({ extended: true }));
+
+app.set('')
+
 app.get('/', (req, res) => {
   res.render('form.ejs')
 });
 
 app.post('/results', (req, res) => {
-    console.log("req.query results: ", req.query);
+  // req.body is required with parser
+    console.log(req.body);
     res.render('results.ejs')
 })
 
