ac499b9d18e3a704c1e2afcc91e60e39549710f5
Shar McLeod <shr.mcl@gmail.com>
2021-07-03 13:23:52 -0500

    changed port to 3001 to use with todoReactHooks app.

--- todos/todosWithServerRedo/server/server.js
+++ todos/todosWithServerRedo/server/server.js
@@ -72,5 +72,5 @@ app.delete('/todos/:id', (req, res) => {
     res.json(toDoArray);
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, ()=> console.log(`listening on port ${port}.`))
