ba060fa53aa0c913c82a039db2e346a5e6da6500
Shar McLeod <shr.mcl@gmail.com>
2021-07-16 23:49:36 -0500

    added method=-override for HTML form requests

--- extras/coderVox_msgBoard/app.js
+++ extras/coderVox_msgBoard/app.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 // connect our routes we created in the posts.js file
 const postRoutes = require("./routes/posts");
 const port = process.env.PORT || 3000;
-
-
+// to use PUT request with HTML form (override the GET/POST-only nature of HTML forms)
+const methodOverride = require("method-override");
 
 // copying from todo app (as well as my config folder)
 // REQUIRE KEYS
@@ -14,8 +14,6 @@ mongoose.connect(keys.mongoURI,{useNewUrlParser: true,useUnifiedTopology: true})
 .then(() => console.log('Connected to database'))
 .catch(error => console.log("Cannot connect to DB"));
 
-
-
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 // parse the body (maybe deprecated or built-in to express?)
@@ -23,7 +21,7 @@ app.use(express.urlencoded({extended: true}));
 // first arg adds "/posts" to start of each route
 // second arg tells app to use routes in posts.js via postRoutes const above
 app.use("/posts", postRoutes);
-
+app.use(methodOverride("_method"))
 
 app.get("/", (req, res) => {
     // res.send('connected')
