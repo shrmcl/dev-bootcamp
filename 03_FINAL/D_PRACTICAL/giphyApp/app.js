2a3642ab9dffb5e42a36e40aef9a8ab7ac17bc30
Shar McLeod <shr.mcl@gmail.com>
2021-07-24 14:54:10 -0500

    the 3 imgage on home.ejs do not show up. but do point to correct src.

--- 03_FINAL/D_PRACTICAL/giphyApp/app.js
+++ 03_FINAL/D_PRACTICAL/giphyApp/app.js
@@ -7,15 +7,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-let myKey = '73H9V6lKlhDik4GLmoJG2CeTj9eTRuUS';
-let trendingUrl = `api.giphy.com/v1/gifs/trending&api_key=${myKey}&limit=5`
-let searchngUrl = `api.giphy.com/v1/gifs/search?q=${searched}&api_key=${myKey}&limit=5`
+let myKey = 'k4YM6ZJZO2TtCxfCt2sDtfT3e2dlevnd';
+let trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${myKey}&limit=3`
+// let searched = '';
+// let searchngUrl = `api.giphy.com/v1/gifs/search?q=${searched}&api_key=${myKey}&limit=5`
 
 app.get('/', (req, res) => {
+  // console.log(trendingUrl)
   $fetch(trendingUrl)
   .then(res => res.json())
   .then(data => {
-
+    // console.log(data.data)
+    // console.log(data.data[0].embed_url)
+    res.render('home', {data: data.data})
   })
   }
 )
