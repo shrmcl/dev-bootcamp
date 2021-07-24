2c01e2da019ec4e83b595639f407f50f249fa0a3
Shar McLeod <shr.mcl@gmail.com>
2021-07-24 15:17:40 -0500

    loading img working with trending api

--- 03_FINAL/D_PRACTICAL/giphyApp/app.js
+++ 03_FINAL/D_PRACTICAL/giphyApp/app.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 const $fetch = require('node-fetch');
 // to parse JSON:
 app.use(express.urlencoded({ extended: false }));
@@ -8,7 +9,7 @@ app.use(express.json());
 
 
 let myKey = 'k4YM6ZJZO2TtCxfCt2sDtfT3e2dlevnd';
-let trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${myKey}&limit=3`
+let trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${myKey}&limit=6`
 // let searched = '';
 // let searchngUrl = `api.giphy.com/v1/gifs/search?q=${searched}&api_key=${myKey}&limit=5`
 
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
   $fetch(trendingUrl)
   .then(res => res.json())
   .then(data => {
-    // console.log(data.data)
+    // console.log(data.data[0].images.downsized.url)
     // console.log(data.data[0].embed_url)
     res.render('home', {data: data.data})
   })
